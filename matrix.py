f987e0273458ec02870d31bf7ccbfa1a831a90a6
Andrew Shao <ashao1@stuy.edu>
2019-02-13 15:35:45 -0500

    lol

--- matrix.py
+++ matrix.py
@@ -12,16 +12,36 @@ import math
 #print the matrix such that it looks like
 #the template in the top comment
 def print_matrix( matrix ):
+    for r in range(4):
+        line = ""
+        for c in matrix:
+            line += c[r] + "  "
+        print(line)
     pass
 
-#turn the paramter matrix into an identity matrix
+#turn the parameter matrix into an identity matrix
 #you may assume matrix is square
 def ident( matrix ):
+    for x in range(4):
+        for y in range(4):
+            if x == y:
+                matrix[x][y] = 1
+            else:
+                matrix[x][y] = 0
     pass
 
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
+    for col in range(len(m2)):
+        tmp = []
+        for i in m2[col]:
+            tmp.append(i)
+        for r in range(4):
+            sum = 0
+            for c in range(4):
+                sum += m2[col][r] * m1[c][r]
+        
     pass
 
 
